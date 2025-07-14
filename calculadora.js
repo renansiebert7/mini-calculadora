767b7f08abf8764070e2d23a2dba842bca58ebb8
renansiebert7 <renansiebert01@gmail.com>
2025-07-14 20:37:45 -0300

    return

--- calculadora.js
+++ calculadora.js
@@ -1,31 +1,35 @@
-function cliqueinaadição(){
+function formatarNumero(numero) {
+    return numero.toLocaleString('pt-BR', { minimumFractionDigits: 0, maximumFractionDigits: 2 });
+}
+
+function cliqueinaadição() {
     const num1 = Number(document.getElementById('input1').value);
     const num2 = Number(document.getElementById('input2').value);
     const resultado = num1 + num2;
-    document.querySelector('.resultado').innerText = resultado;
+    document.querySelector('.resultado').innerText = formatarNumero(resultado);
 }
 
-function cliqueinasubtração(){
+function cliqueinasubtração() {
     const num1 = Number(document.getElementById('input1').value);
     const num2 = Number(document.getElementById('input2').value);
     const resultado = num1 - num2;
-    document.querySelector('.resultado').innerText = resultado;
+    document.querySelector('.resultado').innerText = formatarNumero(resultado);
 }
 
-function cliqueinamultiplicação(){
+function cliqueinamultiplicação() {
     const num1 = Number(document.getElementById('input1').value);
     const num2 = Number(document.getElementById('input2').value);
     const resultado = num1 * num2;
-    document.querySelector('.resultado').innerText = resultado;
+    document.querySelector('.resultado').innerText = formatarNumero(resultado);
 }
 
-function cliqueinadivisão(){
+function cliqueinadivisão() {
     const num1 = Number(document.getElementById('input1').value);
     const num2 = Number(document.getElementById('input2').value);
-     if (num2 === 0) {
-                document.querySelector('.resultado').innerText = 'Erro: Divisão por zero!';
-            } else {
-                const resultado = num1 / num2;
-                document.querySelector('.resultado').innerText = resultado;
-            }
+    if (num2 === 0) {
+        document.querySelector('.resultado').innerText = 'Erro: Divisão por zero!';
+    } else {
+        const resultado = num1 / num2;
+        document.querySelector('.resultado').innerText = formatarNumero(resultado);
+    }
 }
